d6d8d91e3df92721f796eea49bf0dbbe252d88f0
Naresh Kumar <82438404+flyhoneytrap@users.noreply.github.com>
2023-06-01 18:44:47 +0530

    Update number_guessing.py
    canged the turn selecting process.

--- number_guessing.py
+++ number_guessing.py
@@ -1,6 +1,19 @@
 import random
-print("Welcome to number guessing game.\nYou have to guess the number which computer has selected.\nThere are 4 difficulties. \nEasy, Normal and Hard.\nIn easy mode you will have 20 guesses\nIn Normal mode you will have 10 guesses\nIn Hard mode you will have only 5 guesses.\nIn Extreme mode you will have only 1 guess.\nThere is a 100 guesses mode too. You get 100 guesses to guess number.\n\nGood Luck.\n\nNumber will range from 1 to 100.\n\nFor 100 Guess mode enter 0.\nFor Easy mode Enter 1\nFor Normalj mode Enter 2\nFor Hard mode Enter 3\nFor Extreme mode Enter 4\nYour Choice: ")
-
+print("Welcome to number guessing game.\
+      \nYou have to guess the number which computer has selected.\
+      \nThere are 4 difficulties. \nEasy, Normal and Hard.\
+      \nIn easy mode you will have 20 guesses\
+      \nIn Normal mode you will have 10 guesses\
+      \nIn Hard mode you will have only 5 guesses.\
+      \nIn Extreme mode you will have only 1 guess.\
+      \nThere is a 100 guesses mode too. You get 100 guesses to guess number.\
+      \n\nGood Luck.\n\nNumber will range from 1 to 100.\
+      \n\nFor 100 Guess mode enter 0.\
+      \nFor Easy mode Enter 1\
+      \nFor Normalj mode Enter 2\
+      \nFor Hard mode Enter 3\
+      \nFor Extreme mode Enter 4\
+      \nYour Choice: ")
 while True:
     try:
         difficulty = int(input())
@@ -8,23 +21,11 @@ while True:
             break
         print("\nChoice Doesn't Exists. Try again: ")
         continue
-
     except Exception as e:
         print("\nEnter a valid Choice.\n")
-
-
-
 ran_num = int(random.randrange(101))
-if difficulty==1:
-    turn = 20
-elif difficulty == 2:
-    turn = 10
-elif difficulty == 3:
-    turn = 5
-elif difficulty == 4:
-    turn = 1
-elif difficulty == 0:
-    turn = 100
+diff = {0:100,1:20,2:10,3:5,4:1}
+diff[difficulty] = turn
 score = 1
 # print(ran_num)
 print("Guesses Remaining:",turn)
@@ -44,6 +45,4 @@ while turn > 0:
     elif guess > ran_num:
         print("Go Lower. Guesses remaining",turn)
     score += 1
-
-
 print("You didn't guess correctly. The number was",ran_num)
